2b1377c05a65bca1be1353454e739db3668351ef
GABRIEL BOZI <gabrielivobozi@gmail.com>
2021-01-12 00:59:59 -0300

    Initial commit

---
+++ Keyboard/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Keyboard.h"
+
+int main()
+{
+	Keyboard keyboard(3, Keyboard::TypeKeyboard::ABNT_2);
+
+	std::string* stringKeyboard = keyboard.stringKeyboard();
+	
+	for (int i = 0; i < (keyboard.getLines() * keyboard.getHeight()); i++)
+	{
+		std::cout << stringKeyboard[i] << std::endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Keyboard
+ Teclado Terminal
